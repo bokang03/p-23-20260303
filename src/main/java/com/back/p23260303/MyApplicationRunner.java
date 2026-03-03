163d1fa3881cefc7e38dd0793961f389256595e2
bokang03 <bogang12345@gmail.com>
2026-03-03 15:43:18 +0900

    feat : ApplicationRunner 조작

---
+++ src/main/java/com/back/p23260303/MyApplicationRunner.java
@@ -0,0 +1,10 @@
+package com.back.p23260303;
+
+//public class MyApplicationRunner implements ApplicationRunner {
+//
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//        System.out.println("MyApplication Running!");
+//    }
+//
+//}
