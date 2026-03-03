f64dd278fa87040b180d1cb3a1d49ecd4594b70f
bokang03 <bogang12345@gmail.com>
2026-03-03 14:16:24 +0900

    init

---
+++ src/test/java/com/back/p23260303/P23260303ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.p23260303;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P23260303ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
